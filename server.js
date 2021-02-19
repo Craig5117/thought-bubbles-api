d8de4a3a3d6be45746726958ff98bb555ca2ca82
Craig5117 <craig.bennett5117@gmail.com>
2021-02-19 11:01:59 -0500

    set up server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// express middleware for parsing body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thought-bubbles', {
+    // enables findOneAndUpdate and findOneAndDelete
+    useFindAndModify: false,
+    // enables the new Mongoose url string parser (old one is deprecated)
+    useNewUrlParser: true,
+    // enables new Mongoose topology engine (old one is deprecated)
+    useUnifiedTopology: true,
+    // removes deprecation warning for ensureIndex, otherwise no changes to code
+    useCreateIndex: true
+});
+
+mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
