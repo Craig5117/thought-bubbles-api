d6d55ad155c6f51741dbfdfeab91612e19afa613
Craig5117 <craig.bennett5117@gmail.com>
2021-02-20 15:57:32 -0500

    set up user model and get route

---
+++ routes/api/user-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers
+} = require('../../controllers/user-controller');
+
+router
+    .route('/')
+    .get(getAllUsers);
+
+
+module.exports = router;
